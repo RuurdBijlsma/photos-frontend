8e22d72796255f7ca43a763a90e0ea1040f5b0a4
Ruurd Bijlsma <ruurd@bijlsma.dev>
2024-10-28 20:28:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Ruurd Photos
+
+Browse photos.
+
+## Development
+
+### Run as dev
+
+```sh
+npm install
+npm run dev
+```
+
+### Lint with ESLint and format with prettier
+
+```sh
+npm run lint&format
+```
---
+++ src/views/main/MainLayout.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <p>Main layout nav:</p>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/explore">Explore</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <div class="router-view">
+    <h2>Router view:</h2>
+    <RouterView />
+  </div>
+
+</template>
+
+<style scoped>
+.router-view {
+  background-color: red;
+}
+</style>
