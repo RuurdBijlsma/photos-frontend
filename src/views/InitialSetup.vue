d11453dc30fb258d0fc95bfdb182595b8f095fc6
Ruurd Bijlsma <ruurd@bijlsma.dev>
2025-03-24 00:00:12 +0100

    Add start of configure section.

--- src/views/InitialSetup.vue
+++ src/views/InitialSetup.vue
@@ -16,101 +16,196 @@
 
       <v-divider class="mt-10 mb-5" />
 
-      <!-- Folders Status Section -->
-      <div class="folder-status-title mb-3">
-        <h3>Folder Status</h3>
-        <v-spacer />
-        <v-btn
-          variant="plain"
-          density="compact"
-          @click="refreshFolderSummary"
-          :loading="refreshLoading"
-          prepend-icon="mdi-refresh"
-          >Refresh
-        </v-btn>
-      </div>
-      <section v-if="folderSummary">
-        <!-- Media Folder -->
-        <full-folder-status
-          :folder="folderSummary.media_folder"
-          env-var="MEDIA_DIR"
-          title-icon="mdi-camera"
-        />
+      <v-stepper
+        v-model="tabIndex"
+        class="stepper"
+        editable
+        :items="['Validate Drives', 'Configure', 'Start Indexing']"
+      >
+        <template v-slot:item.1>
+          <folder-validation />
+        </template>
 
-        <!-- Thumbnails Folder -->
-        <full-folder-status
-          :folder="folderSummary.thumbnails_folder"
-          env-var="THUMBNAILS_DIR"
-          title-icon="mdi-image-multiple"
-        />
+        <template v-slot:item.2>
+          <p class="text-medium-emphasis text-caption mb-3">
+            Select the media folder where your
+            <span :style="{ fontWeight: 700 }">({{ auth.user?.name }})</span>
+            files are located. You can choose the root of the linked media
+            directory or a specific folder. If you invite others, their media
+            will be kept in separate folders.
+          </p>
+          <div class="folder-picker">
+            <v-divider class="mt-2 mb-2" />
+            <div class="picker-header">
+              <div class="header-buttons">
+                <v-btn
+                  class="mr-2"
+                  title="Go forward"
+                  variant="text"
+                  :disabled="!forwardEnabled"
+                  density="compact"
+                  icon="mdi-arrow-left"
+                />
+                <v-btn
+                  class="mr-2"
+                  title="Go back"
+                  variant="text"
+                  :disabled="!backEnabled"
+                  density="compact"
+                  icon="mdi-arrow-right"
+                />
+                <v-btn
+                  class="mr-2"
+                  variant="text"
+                  :disabled="!upEnabled"
+                  title="Move up a folder."
+                  density="compact"
+                  icon="mdi-arrow-up"
+                />
+                <v-dialog max-width="500">
+                  <template v-slot:activator="{ props: activatorProps }">
+                    <v-btn
+                      class="mr-2"
+                      variant="text"
+                      title="Create folder"
+                      density="compact"
+                      icon="mdi-folder-plus-outline"
+                      v-bind="activatorProps"
+                    />
+                  </template>
 
-        <!-- Media Files Section -->
-        <media-sample :summary="folderSummary" :images="samples" />
+                  <template v-slot:default="{ isActive }">
+                    <v-card title="Create folder">
+                      <v-card-text>
+                        <v-text-field
+                          label="Folder name"
+                          placeholder="Enter folder name"
+                          prepend-icon="mdi-folder-plus-outline"
+                          variant="outlined"
+                          :hide-details="true"
+                          rounded
+                          color="primary"
+                          base-color="rgba(0,0,0,0.5)"
+                        />
+                      </v-card-text>
+                      <v-card-actions>
+                        <v-btn @click="isActive=false">
+                          Create
+                        </v-btn>
+                      </v-card-actions>
+                    </v-card>
+                  </template>
+                </v-dialog>
+              </div>
+              <div class="current-route-display">
+                <div class="route-component route-root">Media Folder</div>
+                <v-icon icon="mdi-chevron-right" />
+                <div class="route-component">Ruurd</div>
+              </div>
+              <div class="header-buttons">
+                <v-btn
+                  class="ml-2"
+                  variant="text"
+                  density="compact"
+                  icon="mdi-refresh"
+                  :loading="refreshLoading"
+                />
+              </div>
+            </div>
+            <v-divider class="mt-2 mb-2" />
+            <div class="picker-entries">
+              <v-list-item
+                rounded
+                @dblclick="openFolder('folder1')"
+                @click="selectFolder('folder1')"
+                prepend-icon="mdi-folder-outline"
+                title="folder1"
+              ></v-list-item>
+              <v-list-item
+                rounded
+                @dblclick="openFolder('folder1')"
+                @click="selectFolder('folder2')"
+                prepend-icon="mdi-folder-outline"
+                title="folder2"
+              ></v-list-item>
+              <v-list-item
+                rounded
+                @dblclick="openFolder('folder1')"
+                @click="selectFolder('folder3')"
+                prepend-icon="mdi-folder-check"
+                class="selected-folder"
+                title="folder3"
+              ></v-list-item>
+            </div>
+            <div class="show-selected-folder">
+              <h3>Selected folder:</h3>
+              <v-chip>/Ruurd/folder3</v-chip>
+            </div>
+          </div>
+        </template>
 
-        <!-- Unsupported Files -->
-        <unsupported-files
-          v-if="folderSummary.unsupported_count > 0"
-          :summary="folderSummary"
-        />
-
-        <!-- Inaccessible Files and Folders -->
-        <inaccessible-entries
-          v-if="folderSummary.inaccessible_entries.length > 0"
-          :summary="folderSummary"
-        />
-      </section>
-
-      <v-skeleton-loader
-        :loading="refreshLoading"
-        v-else
-        type="card-avatar, heading, paragraph, divider, heading, button, card, article, card"
-      ></v-skeleton-loader>
+        <template v-slot:item.3>
+          <v-card title="Step Three" flat>...</v-card>
+        </template>
+      </v-stepper>
     </div>
   </v-main>
 </template>
 
 <script setup lang="ts">
-import { photosApi } from '@/utils/api/PhotosApi'
-import type { FileCountResponse } from '@/utils/api/types'
-import { type Ref, ref } from 'vue'
-import FullFolderStatus from '@/components/setup/FullFolderStatus.vue'
-import MediaSample from '@/components/setup/MediaSample.vue'
-import UnsupportedFiles from '@/components/setup/UnsupportedFiles.vue'
-import InaccessibleEntries from '@/components/setup/InaccessibleEntries.vue'
+import FolderValidation from '@/views/main/FolderValidation.vue'
+import { useAuthStore } from '@/stores/auth'
+import { ref, watch } from 'vue'
+import {
+  type RouteLocationNormalizedLoadedGeneric,
+  useRoute,
+  useRouter,
+} from 'vue-router'
 
-const folderSummary: Ref<FileCountResponse | null> = ref(null)
+const router = useRouter()
+const route = useRoute()
+const auth = useAuthStore()
+
+const upEnabled = ref(false)
+const forwardEnabled = ref(false)
+const backEnabled = ref(false)
 const refreshLoading = ref(false)
-let N_SAMPLES = 8
-const samples: Ref<string[]> = ref(Array(N_SAMPLES))
 
-async function refreshFolderSummary() {
-  refreshLoading.value = true
-  try {
-    const result = await photosApi.validateFolders()
-    if ('error' in result) {
-      console.warn('error getting validate folders result', result)
-    } else {
-      console.log(result)
-      folderSummary.value = result
-      N_SAMPLES = result.samples.length
-      let j = 0
-      for (let i = 0; i < N_SAMPLES; i++) {
-        getImageUrl(result.samples[i]).then(
-          imageUrl => (samples.value[j++] = imageUrl),
-        )
-      }
-    }
-  } finally {
-    refreshLoading.value = false
-  }
+function openFolder(folder: string) {
+  console.log('open', folder)
 }
 
-async function getImageUrl(file: string): Promise<string> {
-  console.log(file)
-  return await photosApi.rawMediaUrl(file)
+function selectFolder(folder: string) {
+  console.log('select', folder)
 }
 
-refreshFolderSummary().then()
+function getStepFromRoute(
+  r: RouteLocationNormalizedLoadedGeneric,
+): number | null {
+  const stepString = r.query.step?.toString()
+  if (!stepString) return null
+  const step = parseInt(stepString)
+  if (step > 3 || step < 1) return null
+  return step
+}
+
+function setStepFromRoute() {
+  const step = getStepFromRoute(route)
+  if (step === null || tabIndex.value === step) return
+  tabIndex.value = step
+}
+
+const tabIndex = ref(0)
+watch(tabIndex, () => {
+  console.log(tabIndex.value)
+  router.push({
+    query: {
+      step: tabIndex.value,
+    },
+  })
+})
+watch(route, setStepFromRoute)
+setStepFromRoute()
 </script>
 
 <style scoped>
@@ -134,7 +229,7 @@ refreshFolderSummary().then()
   overflow: hidden;
   box-shadow: 0 10px 20px 0 rgba(0, 0, 0, 0.07);
   max-width: 800px;
-  padding: 50px 60px;
+  padding: 30px 40px;
   margin: 100px auto 0;
   transition: box-shadow 0.3s ease;
 }
@@ -146,6 +241,8 @@ refreshFolderSummary().then()
 .title-box {
   display: flex;
   gap: 40px;
+  padding: 20px;
+  padding-bottom: 0;
 }
 
 .big-image {
@@ -173,18 +270,44 @@ refreshFolderSummary().then()
   font-weight: 600;
 }
 
-.container h3 {
-  opacity: 0.7;
-  font-weight: 500;
-  font-size: 20px;
+.container:deep(.v-sheet) {
+  background-color: transparent !important;
+  box-shadow: none !important;
 }
 
-.folder-status-title {
+.container:deep(.v-stepper-header) {
+  box-shadow: none !important;
+}
+
+.picker-header {
   display: flex;
+  align-items: center;
 }
 
-.black-text {
+.current-route-display {
+  display: flex;
+  background-color: rgba(0, 0, 0, 0.05);
+  border-radius: 5px;
+  padding: 4px 15px;
+  flex-grow: 1;
+  font-size: 14px;
+  font-weight: 500;
+}
+
+.header-buttons {
+  opacity: 0.6;
+}
+
+.selected-folder {
+  background-color: rgba(0, 0, 0, 0.05);
+}
+
+.show-selected-folder{
+  display: flex;
+  gap: 25px;
+  align-items: center;
+  margin-top: 20px;
   font-weight: 600;
-  color: rgba(79, 49, 26, 0.7);
+  font-size: 14px;
 }
 </style>
---
+++ src/views/main/FolderValidation.vue
@@ -0,0 +1,113 @@
+<script setup lang="ts">
+import FullFolderStatus from '@/components/setup/FullFolderStatus.vue'
+import UnsupportedFiles from '@/components/setup/UnsupportedFiles.vue'
+import InaccessibleEntries from '@/components/setup/InaccessibleEntries.vue'
+import MediaSample from '@/components/setup/MediaSample.vue'
+import { photosApi } from '@/utils/api/PhotosApi'
+import type { FileCountResponse } from '@/utils/api/types'
+import { type Ref, ref } from 'vue'
+
+const folderSummary: Ref<FileCountResponse | null> = ref(null)
+const refreshLoading = ref(false)
+let N_SAMPLES = 8
+const samples: Ref<string[]> = ref(Array(N_SAMPLES))
+
+async function refreshFolderSummary() {
+  refreshLoading.value = true
+  try {
+    const result = await photosApi.validateFolders()
+    if ('error' in result) {
+      console.warn('error getting validate folders result', result)
+    } else {
+      console.log(result)
+      folderSummary.value = result
+      refreshLoading.value = false
+
+      N_SAMPLES = result.samples.length
+      let j = 0
+      for (let i = 0; i < N_SAMPLES; i++) {
+        getImageUrl(result.samples[i]).then(
+          imageUrl => (samples.value[j++] = imageUrl),
+        )
+      }
+    }
+  } finally {
+    refreshLoading.value = false
+  }
+}
+
+async function getImageUrl(file: string): Promise<string> {
+  console.log(file)
+  return await photosApi.rawMediaUrl(file)
+}
+
+refreshFolderSummary().then()
+</script>
+
+<template>
+  <!-- Folders Status Section -->
+  <div class="folder-status-title mb-3">
+    <p class="text-caption text-medium-emphasis">
+      Make sure the correct folders are linked before starting the indexing
+      process.
+    </p>
+    <v-spacer />
+    <v-btn
+      variant="plain"
+      density="compact"
+      @click="refreshFolderSummary"
+      :loading="refreshLoading"
+      prepend-icon="mdi-refresh"
+      >Refresh
+    </v-btn>
+  </div>
+  <section v-if="folderSummary">
+    <!-- Media Folder -->
+    <full-folder-status
+      :folder="folderSummary.media_folder"
+      env-var="MEDIA_DIR"
+      title-icon="mdi-camera"
+    />
+
+    <!-- Thumbnails Folder -->
+    <full-folder-status
+      :folder="folderSummary.thumbnails_folder"
+      env-var="THUMBNAILS_DIR"
+      title-icon="mdi-image-multiple"
+    />
+
+    <!-- Media Files Section -->
+    <media-sample :summary="folderSummary" :images="samples" />
+
+    <!-- Unsupported Files -->
+    <unsupported-files
+      v-if="folderSummary.unsupported_count > 0"
+      :summary="folderSummary"
+    />
+
+    <!-- Inaccessible Files and Folders -->
+    <inaccessible-entries
+      v-if="folderSummary.inaccessible_entries.length > 0"
+      :summary="folderSummary"
+    />
+  </section>
+
+  <v-skeleton-loader
+    :loading="refreshLoading"
+    v-else
+    type="card-avatar, heading, paragraph, divider, heading, button, card, article, card"
+  ></v-skeleton-loader>
+</template>
+
+<style scoped>
+.folder-status-title {
+  display: flex;
+  align-items: center;
+}
+
+h3 {
+  opacity: 0.7;
+  font-weight: 500;
+  font-size: 20px;
+}
+</style>
