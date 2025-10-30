74f7d40a7058726283d51c258ead7ccf07c3a8e9
Ruurd Bijlsma <ruurd@bijlsma.dev>
2025-10-30 22:01:11 +0100

    Fix GridRowHeader.vue

---
+++ src/components/photo-grid/GridRowHeader.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { RowLayout } from '@/components/photo-grid/GridRow.vue'
+
+defineProps<{
+  row: RowLayout
+}>()
+</script>
+
+<template>
+  <h1 class="month-title">{{ row.monthId }}</h1>
+</template>
+
+<style scoped>
+.month-title {
+  padding: 20px 0 20px 15px;
+  margin-left: 20px;
+}
+</style>
