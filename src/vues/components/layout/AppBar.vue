fbbe54a8152d8f3b529d40ab603661d6e8a93014
Ruurd Bijlsma <ruurd@bijlsma.dev>
2026-05-08 12:56:52 +0200

    Add user menu in AppBar.vue

---
+++ src/vues/components/layout/AppBar.vue
@@ -0,0 +1,99 @@
+<script setup lang="ts">
+import SearchBar from '@/vues/components/ui/SearchBar.vue'
+import { useRouter } from 'vue-router'
+
+function logout() {
+  localStorage.clear()
+  location.reload()
+}
+</script>
+
+<template>
+  <v-app-bar density="comfortable" :height="70" class="header" color="transparent" elevation="0">
+    <h1 class="appbar-title"><span>Ruurd</span> Photos</h1>
+    <v-spacer />
+    <search-bar />
+    <v-spacer />
+    <div class="header-buttons">
+      <v-btn variant="plain" rounded>
+        <v-icon icon="mdi-upload"></v-icon>
+        Upload
+      </v-btn>
+      <v-menu>
+        <template v-slot:activator="{ props }">
+          <v-btn icon v-bind="props">
+            <v-avatar>
+              <v-img src="img/avatar.jpg"></v-img>
+            </v-avatar>
+          </v-btn>
+        </template>
+        <div>
+          <v-sheet color="background">
+            <div class="menu-header">
+              <div class="user-icon">
+                <v-avatar>
+                  <v-img src="img/avatar.jpg"></v-img>
+                </v-avatar>
+              </div>
+              <div class="user-info">
+                <p class="user-name">Ruurd</p>
+                <p class="user-email">ruurd@bijlsma.dev</p>
+              </div>
+            </div>
+            <v-list color="background">
+              <v-list-item prepend-icon="mdi-account-circle" to="/me">
+                <v-list-item-title>Profile</v-list-item-title>
+              </v-list-item>
+              <v-list-item prepend-icon="mdi-logout" @click="logout">
+                <v-list-item-title>Sign out</v-list-item-title>
+              </v-list-item>
+              <v-divider />
+              <v-list-item prepend-icon="mdi-cog" to="/settings">
+                <v-list-item-title>Settings</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-sheet>
+        </div>
+      </v-menu>
+    </div>
+  </v-app-bar>
+</template>
+
+<style scoped>
+.appbar-title {
+  font-weight: 600;
+  font-size: 20px;
+  margin-left: 50px;
+  opacity: 0.8;
+}
+
+.appbar-title > span {
+  font-weight: 400;
+}
+
+.header-buttons {
+  display: flex;
+  gap: 20px;
+  align-items: center;
+}
+
+.menu-header {
+  padding: 20px;
+  padding-bottom: 0;
+  display: flex;
+  gap: 20px;
+  align-items: center;
+}
+
+.user-info p {
+  margin: 0;
+}
+
+.user-name {
+  font-weight: bold;
+}
+
+.user-email {
+  opacity: 0.7;
+}
+</style>
